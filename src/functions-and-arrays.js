49c9da55c57fc1d74ed81df82b38730f0dfe3930
Josip Milan <josip.milan@aol.com>
2023-11-23 15:49:04 +0100

    Functions & Arrays first Go

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,60 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
+let result = maxOfTwoNumbers(3, 9);
+console.log(result);
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
 
+  let longestWord = words[0];
 
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
 
+  return longestWord;
+}
+  
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+  }
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+function sum(mixedArr) {
+  let sum = 0;
+
+  for (let i = 0; i < mixedArr.length; i++) {
+    sum +- mixedArr[i]
+  }
+  return sum;
+}
 
 
 
@@ -26,13 +62,39 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+function averageNumbers(numbers) {
+  if (numbers.length === 0) {
+    return null;
+  }
+  const totalSum = sumNumbers(numbers);
+  const average = totalSum / numbers.length;
 
+  return average;
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(words) {
+  if (words.length === 0) {
+    return null;
+  }
+  let totalLength = 0; 
+  for(let i = 0; i < words.length; i++){
+    totalLength += words[i].length;
+  }
+const averageLength = totalLength / words.length;
+
+return averageLength;
+
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
